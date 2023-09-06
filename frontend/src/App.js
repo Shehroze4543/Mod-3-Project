3e09e5312d70150e8740acb0cd79fce4b26c357e
Shehroze4543 <86188771+Shehroze4543@users.noreply.github.com>
2023-09-06 14:15:14 -0400

    created login and sign up forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 // pages & components
+// pages & components
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+//import Home from "./pages/Home";
 function App() {
   return (
     <div className="App">
@@ -11,6 +16,8 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </BrowserRouter>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,9 +7,38 @@ const Navbar = () => {
         <Link to="/">
           <h1>Calorie tracker</h1>
         </Link>
+        <nav>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>
       </div>
     </header>
   );
 };
 
 export default Navbar;
+
+// import { Link } from 'react-router-dom'
+
+// const Navbar = () => {
+
+//   return (
+//     <header>
+//       <div className="container">
+//         <Link to="/">
+//           <h1>Workout Buddy</h1>
+//         </Link>
+//         <nav>
+//           <div>
+//             <Link to="/login">Login</Link>
+//             <Link to="/signup">Signup</Link>
+//           </div>
+//         </nav>
+//       </div>
+//     </header>
+//   )
+// }
+
+// export default Navbar
