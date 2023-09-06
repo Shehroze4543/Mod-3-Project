e8dc8766d68214eff40c2d5206bde9f2ad8b1854
Shehroze4543 <86188771+Shehroze4543@users.noreply.github.com>
2023-09-06 14:59:36 -0400

    protected API routes

--- backend/routes/meals.js
+++ backend/routes/meals.js
@@ -7,7 +7,15 @@ const {
   deleteMeal,
   updateMeal,
 } = require("../controllers/mealController");
+
+
+const requireAuth = require('../middleware/requireAuth')
+
 const router = express.Router();
+
+// require auth for all workout routes
+router.use(requireAuth)
+
 
 // GET ALL MEALS
 router.get("/", getMeals);
