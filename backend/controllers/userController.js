4c361abd45faad1ccc48532d37883ccab61bd800
Shehroze4543 <86188771+Shehroze4543@users.noreply.github.com>
2023-09-06 13:42:43 -0400

    added signing tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,60 +1,29 @@
-const User = require("../models/userModel");
-//const jwt = require("jsonwebtoken");
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
-// const createToken = (_id) => {
-//   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
-// };
-// // login user
-// const loginUser = async (req, res) => {
-//   const { email, password } = req.body;
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '5d' })
+}
 
-//   try {
-//     const user = await User.login(email, password);
-
-//     // create a token
-//     const token = createToken(user._id);
-
-//     res.status(200).json({ email, token });
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-//   // res.json({ mssg: "LOGIN USER" });
-// };
-
-// // Signup User
-// const signupUser = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await User.signup(email, password);
-
-//     // create a token
-//     const token = createToken(user._id);
-
-//     res.status(200).json({ email, token });
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-//   //res.json({ mssg: "Signup USER" });
-// };
 // login a user
 const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
-};
+  res.json({mssg: 'login user'})
+}
 
 // signup a user
 const signupUser = async (req, res) => {
-  const { email, password } = req.body;
+  const {email, password} = req.body
 
   try {
-    const user = await User.signup(email, password);
+    const user = await User.signup(email, password)
 
-    res.status(200).json({ email, user });
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({error: error.message})
   }
-};
-module.exports = {
-  signupUser,
-  loginUser,
-};
+}
+
+module.exports = { signupUser, loginUser }
