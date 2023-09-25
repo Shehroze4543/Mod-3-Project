eb9c5603829d7d8ba6a983941c694a12b953f650
Shehroze4543 <86188771+Shehroze4543@users.noreply.github.com>
2023-09-24 20:35:59 -0400

    added npm run build

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,45 +1,47 @@
-import { Link } from "react-router-dom";
-import { useLogout } from "../hooks/useLogout";
-import { useAuthContext } from "../hooks/useAuthContext";
+import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Navbar = () => {
-  const { logout } = useLogout();
-  const { user } = useAuthContext();
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   const handleClick = () => {
-    logout();
-  };
+    logout()
+  }
 
   return (
-    <header className="header">
-   
-        <div className="header1"> 
-          <Link to="/">
-          <h1 className="title">YourFitnessPal</h1>
+    <header className='header'>
+      <div className='header1'>
+        <Link to='/'>
+          <h1 className='title'>YourFitnessPal</h1>
         </Link>
-        </div>
- 
-     
-       
-        <div className="header2">  
-          {user && (
-            <div className="top-right">
-              <span className="heading-box">Hello, {user.email.split('@')[0]}</span>
-              <button className="btn" onClick={handleClick}>Log out</button>
-            </div>
-          )}
-          {!user && (
-            <div className="top-right">
-              <Link className="heading-box" to="/login">Login</Link>
-              <Link className="heading-box" to="/signup">Signup</Link>
-            </div>
-          )}
-        
-        </div>
-      
-     
-    </header>
-  );
-};
+      </div>
 
-export default Navbar;
+      <div className='header2'>
+        {user && (
+          <div className='top-right'>
+            <span className='heading-box'>
+              Hello, {user.email.split('@')[0]}
+            </span>
+            <button className='btn' onClick={handleClick}>
+              Log out
+            </button>
+          </div>
+        )}
+        {!user && (
+          <div className='top-right'>
+            <Link className='heading-box' to='/login'>
+              Login
+            </Link>
+            <Link className='heading-box' to='/signup'>
+              Signup
+            </Link>
+          </div>
+        )}
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
