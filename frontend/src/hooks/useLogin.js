a6cd8d45037c21267bc2179ea5cdcdf706ef3300
Shehroze4543 <86188771+Shehroze4543@users.noreply.github.com>
2023-09-27 20:45:31 -0400

    changed the proxy

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,20 +1,23 @@
-import { useState } from "react";
-import { useAuthContext } from "./useAuthContext";
+import {useState} from "react";
+import {useAuthContext} from "./useAuthContext";
 
 export const useLogin = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
-  const { dispatch } = useAuthContext();
+  const {dispatch} = useAuthContext();
 
   const login = async (email, password) => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("/api/user/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+    const response = await fetch(
+      "https://mern-full-stack-application.onrender.com/api/user/login",
+      {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password}),
+      }
+    );
     const json = await response.json();
 
     if (!response.ok) {
@@ -26,12 +29,12 @@ export const useLogin = () => {
       localStorage.setItem("user", JSON.stringify(json));
 
       // update the auth context
-      dispatch({ type: "LOGIN", payload: json });
+      dispatch({type: "LOGIN", payload: json});
 
       // update loading state
       setIsLoading(false);
     }
   };
 
-  return { login, isLoading, error };
+  return {login, isLoading, error};
 };
